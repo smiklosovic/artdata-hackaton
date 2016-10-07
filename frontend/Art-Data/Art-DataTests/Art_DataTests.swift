d833b52727e5f4897e9e1576bc9ed5e4c0466cec
Alexey Potapov <a.potapov@thirtyseventy.digital>
2016-10-07 22:55:44 +0200

    gitignore added. frontend. project created,

---
+++ frontend/Art-Data/Art-DataTests/Art_DataTests.swift
@@ -0,0 +1,36 @@
+//
+//  Art_DataTests.swift
+//  Art-DataTests
+//
+//  Created by Alexey Potapov on 07/10/2016.
+//  Copyright © 2016 Community. All rights reserved.
+//
+
+import XCTest
+@testable import Art_Data
+
+class Art_DataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
